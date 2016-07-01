f63c81730cd1335002089db5c12d0945d83968a5
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2016-07-01 09:49:51 -0400

    huh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker build -t sjoeboo/baseimage_centos:7 centos_7/
-docker build -t sjoeboo/baseimage_centos:6 centos_6/
+docker build --no-cache=true --rm=true -t sjoeboo/baseimage_centos:7 centos_7/
+docker build --no-cache=true --rm=true -t sjoeboo/baseimage_centos:6 centos_6/
+docker images -q --filter "dangling=true" | xargs docker rmi
